1c770df4435985ca822b2eebc57be1ce0afd5ab9
Keyther34 <156479261+Keyther34@users.noreply.github.com>
2024-01-25 17:06:11 -0400

    Add files via upload

---
+++ conexion.php
@@ -0,0 +1,16 @@
+<?php
+$host = 'localhost';
+$user = 'root';
+$password = '';
+$db = 'facturacion1';
+
+$conection =  @mysqli_connect($host,$user,$password,$db);
+
+//mysqli_close($conection);
+
+if (!$conection){
+	echo "Error en la conexion";
+
+ }
+
+?>
